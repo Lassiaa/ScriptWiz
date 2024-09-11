c938297b5dba1d737a9496408a14941b0507b190
sallyzy0123 <sally_zy@protonmail.com>
2024-09-11 20:03:19 +0300

    add the nav bar

--- scriptwiz/src/App.jsx
+++ scriptwiz/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import FrontPage from "./views/FrontPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar /> 
       <Routes>
         <Route path="/" element={<FrontPage />} />
       </Routes>
