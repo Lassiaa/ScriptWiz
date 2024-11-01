84cdfee7a6965288860a7ef11379d81a9f7f72a5
catkos <catrina.koskinen@gmail.com>
2024-11-01 12:42:03 +0200

    fix hashroute

--- scriptwiz/src/main.jsx
+++ scriptwiz/src/main.jsx
@@ -1,19 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import {FileProvider} from './contexts/fileContext.jsx'
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
